bc8a7787d2724cfabf7eb52795f1b0f38a786412
João Pedro Volponi <jpvrosada@hotmail.com>
2024-07-18 18:44:55 -0300

    feat: 🎸 Alert

--- Login/Login/Alert/Alert.swift
+++ Login/Login/Alert/Alert.swift
@@ -14,4 +14,13 @@ class Alert: NSObject {
     init(controller: UIViewController) {
         self.controller = controller
     }
+    
+    func getAlert(titulo: String, mensagem: String, completion:(() -> Void)? = nil) {
+        let alertController = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        let cancelar = UIAlertAction(title: "OK", style: .cancel) { acao in
+            completion?()
+        }
+        alertController.addAction(cancelar)
+        self.controller.present(alertController, animated: true, completion: nil)
+    }
 }
