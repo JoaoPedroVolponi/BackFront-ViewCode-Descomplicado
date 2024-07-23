786804bfb921350f15ab54b60caa63b5e0b56318
João Pedro Volponi <jpvrosada@hotmail.com>
2024-07-23 19:39:41 -0300

    feat: 🎸 Home files

---
+++ Login/Login/Home/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 23/07/24.
+//
+
+import Foundation
+import UIKit
+
+class HomeScreen: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Login/Login/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 23/07/24.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .red
+    }
+}
