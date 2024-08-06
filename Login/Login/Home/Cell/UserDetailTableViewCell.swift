6c147c37ee13fd9712e7eb9311774be7e7e860e5
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-05 23:10:06 -0300

    files UserDetailTableViewCell

---
+++ Login/Login/Home/Cell/UserDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserDetailTableViewCell.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 05/08/24.
+//
+
+import UIKit
+
+class UserDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
