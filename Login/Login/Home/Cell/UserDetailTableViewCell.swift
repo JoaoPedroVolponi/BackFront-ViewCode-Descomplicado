c1d47d6e1792f3b85af2ee01f3875edb581d3fd3
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-06 18:53:54 -0300

    feat: 🎸 userImageView e nomeLabel

--- Login/Login/Home/Cell/UserDetailTableViewCell.swift
+++ Login/Login/Home/Cell/UserDetailTableViewCell.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class UserDetailTableViewCell: UITableViewCell {
 
+    lazy var userImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nomeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
