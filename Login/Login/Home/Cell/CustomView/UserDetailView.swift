aec9473dad57e1d72a8355da4c4d358e4226f4cc
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-19 22:44:13 -0300

    feat: 🎸 UserDetailView

---
+++ Login/Login/Home/Cell/CustomView/UserDetailView.swift
@@ -0,0 +1,13 @@
+//
+//  UserDetailView.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 16/08/24.
+//
+
+import Foundation
+import UIKit
+
+class UserDetailView: UITableViewCell {
+    
+}
