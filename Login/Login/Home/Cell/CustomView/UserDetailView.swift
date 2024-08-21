f323bfb3841de9a2681cc07225d74b3320f134bf
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-20 23:25:09 -0300

    feat: 🎸 UserDetailView elementos

--- Login/Login/Home/Cell/CustomView/UserDetailView.swift
+++ Login/Login/Home/Cell/CustomView/UserDetailView.swift
@@ -8,6 +8,29 @@
 import Foundation
 import UIKit
 
-class UserDetailView: UITableViewCell {
+class UserDetailView: UIView {
     
+    //MARK: Elementos
+    lazy var userImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        return label
+    }()
+    
+    
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
