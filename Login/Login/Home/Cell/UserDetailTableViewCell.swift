b81d1cc1729e9eda514ee7212868715c680ddd73
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-20 23:29:19 -0300

    feat: 🎸 Separando dependencias

--- Login/Login/Home/Cell/UserDetailTableViewCell.swift
+++ Login/Login/Home/Cell/UserDetailTableViewCell.swift
@@ -10,50 +10,19 @@ import UIKit
 class UserDetailTableViewCell: UITableViewCell {
     
     static let identifier: String = "UserDetailTableViewCell"
-    
-//    lazy var userImageView: UIImageView = {
-//        let image = UIImageView()
-//        image.translatesAutoresizingMaskIntoConstraints = false
-//        image.contentMode = .scaleAspectFit
-//        return image
-//    }()
-//    
-//    lazy var nameLabel: UILabel = {
-//        let label = UILabel()
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        label.textColor = .black
-//        return label
-//    }()
-//    
-//    func addSubView() {
-//        self.contentView.addSubview(userImageView)
-//        self.contentView.addSubview(nameLabel)
-//    }
-//    
+      
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        self.addSubView()
-        self.setUpContraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func setUpCell(data: DataUser) {
-        self.nameLabel.text = data.name
-        self.userImageView.image = UIImage(named: data.nameImage)
+    func addSubView() {
+        
     }
     
-//    private func setUpContraints() {
-//        NSLayoutConstraint.activate([
-//            userImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-//            userImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-//            userImageView.heightAnchor.constraint(equalToConstant: 80),
-//            userImageView.widthAnchor.constraint(equalToConstant: 80),
-//            
-//            nameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-//            nameLabel.leadingAnchor.constraint(equalTo: userImageView.trailingAnchor, constant: 20)
-//        ])
-//    }
+    public func setUpCell(data: DataUser) {
+    }
 }
