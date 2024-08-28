ddeda9ad9a34f5677f27807d0d9d49162f366c72
João Pedro Volponi <jpvrosada@hotmail.com>
2024-08-27 23:14:03 -0300

    feat: 🎸 SportTableViewCell class

---
+++ Login/Login/Home/Cell/SportTableViewCell/SportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SportTableViewCell.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 27/08/24.
+//
+
+import UIKit
+
+class SportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
