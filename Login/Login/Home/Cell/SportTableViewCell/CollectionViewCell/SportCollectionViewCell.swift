8f0f4a2f437a00e1e28d242124078e924e958e7a
João Pedro Volponi <jpvrosada@hotmail.com>
2024-09-04 23:07:54 -0300

    feat: 🎸 SportCollectionViewCell

---
+++ Login/Login/Home/Cell/SportTableViewCell/CollectionViewCell/SportCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SportCollectionViewCell.swift
+//  Login
+//
+//  Created by João Pedro Volponi on 04/09/24.
+//
+
+import UIKit
+
+class SportCollectionViewCell: UICollectionViewCell {
+    
+}
